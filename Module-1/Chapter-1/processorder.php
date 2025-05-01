35e0a85e113995fd1e86f6d24556f816ea85e09f
404kevnotfound <mewzok7@yahoo.com>
2025-05-01 11:01:40 -0600

    - Assign variables and display quantities

--- Module-1/Chapter-1/processorder.php
+++ Module-1/Chapter-1/processorder.php
@@ -8,6 +8,16 @@
     <h2>Order Results</h2> 
     <?php
       echo "<p>Order processed at ".date('H:i, jS F Y')."</p>";
-    ?>    
+
+      // create short variable names
+      $tireqty = $_POST['tireqty'];
+      $oilqty = $_POST['oilqty'];
+      $sparkqty = $_POST['sparkqty'];
+
+      echo '<p>Your order is as follows: </p>';
+      echo htmlspecialchars($tireqty).' tires<br />';
+      echo htmlspecialchars($oilqty).' bottles of oil<br />';
+      echo htmlspecialchars($sparkqty).' spark plugs<br />';
+    ?>
   </body>
 </html>
