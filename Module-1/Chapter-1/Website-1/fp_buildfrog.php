d9af9c074e7b4b918eacb5a30213385ec798880a
404kevnotfound <mewzok7@yahoo.com>
2025-05-06 12:39:12 -0600

    - Add styling

--- Module-1/Chapter-1/Website-1/fp_buildfrog.php
+++ Module-1/Chapter-1/Website-1/fp_buildfrog.php
@@ -8,13 +8,16 @@
 <html>
   <head>
     <title>Frog Parts - Frog Created</title>
+    <link href="fp_styles.css" rel="stylesheet" />
   </head>
   <body>
-    <h1>Frog Parts</h1>
-    <h2>Frog Created</h2> 
-    <?php
-    // display frog base
-    echo '<img src="fp_images.php?frogcolor='.urlencode($frogcolor).'&frogarm='.urlencode($frogarm).'&frogleg='.urlencode($frogleg).'" alt="Your frog" />';
-    ?>
+      <div class="result-container">
+      <h1>Frog Parts</h1>
+      <h2>Your frog</h2> 
+      <?php
+      // display frog base
+      echo '<img src="fp_images.php?frogcolor='.urlencode($frogcolor).'&frogarm='.urlencode($frogarm).'&frogleg='.urlencode($frogleg).'" alt="Your frog" />';
+      ?>
+    </div>
   </body>
 </html>
