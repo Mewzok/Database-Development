fdfcfbdce95345253b713e243d58f8e3ad5a2cb3
404kevnotfound <mewzok7@yahoo.com>
2025-05-06 11:05:56 -0600

    - Finish part picker and image creation

--- Module-1/Chapter-1/Website-1/fp_buildfrog.php
+++ Module-1/Chapter-1/Website-1/fp_buildfrog.php
@@ -1,6 +1,6 @@
 <?php
   // create short variable names
-  $frogclr = $_POST['frogcolor'];
+  $frogcolor = $_POST['frogcolor'];
   $frogarm = $_POST['frogarm'];
   $frogleg = $_POST['frogleg'];
 ?>
@@ -13,118 +13,8 @@
     <h1>Frog Parts</h1>
     <h2>Frog Created</h2> 
     <?php
-    /* 
-      display frog base
-      check arm
-      display arm
-      check leg
-      display leg
-      change color
-    */
-
     // display frog base
-    echo '<img src="fp_images.php?frogarm='.urlencode($frogarm).'&frogleg='.urlencode($frogleg).'" alt="Your frog" />';
- /*
-    switch($frogarm) {
-      case "armfrog" :
-        echo '<img src="resources/armfrog.png"';
-        break;
-      case "armcrab" :
-        echo '<img src="resources/armcrab.png"';
-        break;
-      case "armdog" :
-        echo '<img src="resources/armdog.png"';
-        break;
-    } 
-
-    // calculate total number of items selected
-      $totalqty = 0;
-      $totalqty = $tireqty + $oilqty + $sparkqty;
-
-      // warn user of no items selected
-      // display number of items selected
-      if($totalqty == 0) {
-        echo '<p style="color:red">';
-        echo "You did not order anything on the previous page!<br />";
-        echo '</p>';
-        exit;
-      } else {
-        echo "<p>Order processed at ".date('H:i, jS F Y')."</p>";
-        echo '<p>Your order is as follows: </p>';
-        if($tireqty > 0)
-          echo htmlspecialchars($tireqty).' tires<br />';
-        if($oilqty > 0)
-          echo htmlspecialchars($oilqty).' bottles of oil<br />';
-        if($sparkqty > 0)
-          echo htmlspecialchars($sparkqty).' spark plugs<br />';
-      }
-
-      // determine tire discount
-      if($tireqty < 10) {
-        $discount = 0;
-      } else if(($tireqty >= 10) && ($tireqty <= 49)) {
-        $discount = 5;
-      } else if(($tireqty >= 50) && ($tireqty <= 99)) {
-        $discount = 10;
-      } else if($tireqty >= 100) {
-        $discount = 15;
-      }
-
-      // determine customer status
-      switch($find) {
-        case "a" :
-          echo "<p>Regular customer.</p>";
-          break;
-        case "b" :
-          echo "<p>Customer referred by TV advert.</p>";
-          break;
-        case "c" :
-          echo "<p>Customer referred by phone directory.</p>";
-          break;
-        case "d" :
-          echo "<p>Customer referred by word of mouth.</p>";
-          break;
-        default :
-          echo "<p>We do not know how this customer found us.</p>";
-          break;
-      }
-
-      // display total number of items ordered
-      echo "<p>Items ordered: ".$totalqty."<br />";
-      $totalamount = 0.00;
-
-      // prices for each item
-      define('TIREPRICE', 100);
-      define('OILPRICE', 10);
-      define('SPARKPRICE', 4);
-
-      // determine tire price before discount
-      $tiretotal = $tireqty * TIREPRICE;
-
-      // apply tire discount if applicable
-      if($discount > 0) {
-        $tiretotal -= $tiretotal * ($discount / 100);
-        echo "Tire discount: ".$discount."%<br />";
-      }
-
-      // calculate total amount
-      $totalamount = $tiretotal + $oilqty * OILPRICE + $sparkqty * SPARKPRICE;
-
-      // display total amount
-      echo "Subtotal: $".number_format($totalamount,2)."<br />";
-
-      // calculate and display total after tax
-      $taxrate = 0.10; // local sales tax is 10%
-      $totalamount = $totalamount * (1 + $taxrate);
-      echo "Total including tax: $".number_format($totalamount,2)."</p>";
-
-      // test isset and empty
-      /*
-      echo 'isset($tireqty): '.isset($tireqty).'<br />';
-      echo 'isset($nothere): '.isset($nothere).'<br />';
-      echo 'empty($tireqty): '.empty($tireqty).'<br />';
-      echo 'empty($nothere): '.empty($nothere).'<br />';
-      */
+    echo '<img src="fp_images.php?frogcolor='.urlencode($frogcolor).'&frogarm='.urlencode($frogarm).'&frogleg='.urlencode($frogleg).'" alt="Your frog" />';
     ?>
   </body>
 </html>
--- Module-1/Chapter-1/Website-1/fp_images.php
+++ Module-1/Chapter-1/Website-1/fp_images.php
@@ -2,21 +2,21 @@
     header('Content-Type: image/png');
 
     // retrieve variables
-    //$color = $_GET['frogcolor'];
+    $color = $_GET['frogcolor'];
     $arm = $_GET['frogarm'];
     $leg = $_GET['frogleg'];
 
-    $basefrog = imagecreatefrompng("resources/frogbasegreen.png");
+    $basefrog = imagecreatefrompng("resources/frogbase{$color}.png");
     imagesavealpha($basefrog, true);
 
     if($arm) {
-        $armimg = imagecreatefrompng("resources/{$arm}.png");
+        $armimg = imagecreatefrompng("resources/{$arm}{$color}.png");
         imagesavealpha($armimg, true);
         imagecopy($basefrog, $armimg, 0, 0, 0, 0, imagesx($armimg), imagesy($armimg));
     }
 
     if($leg) {
-        $legimg = imagecreatefrompng("resources/{$leg}.png");
+        $legimg = imagecreatefrompng("resources/{$leg}{$color}.png");
         imagesavealpha($legimg, true);
         imagecopy($basefrog, $legimg, 0, 0, 0, 0, imagesx($legimg), imagesy($legimg));
     }
