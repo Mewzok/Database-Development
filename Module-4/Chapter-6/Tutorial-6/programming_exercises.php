07fb8ac5f2978058325637f74f1333da0b96c591
404kevnotfound <mewzok7@yahoo.com>
2025-05-20 08:32:55 -0600

    - Add and complete Chapter 6 programming exercises

---
+++ Module-4/Chapter-6/Tutorial-6/programming_exercises.php
@@ -0,0 +1,27 @@
+<?php
+    // exercise 5
+    namespace orders;
+
+    // exercise 1
+    class MyClass
+    {
+        public $attribute1;
+    }
+
+    // exercise 2
+    $c = new MyClass();
+
+    // exercise 3
+    class A {
+
+    }
+
+    class B extends A {
+
+    }
+
+    // exercise 4
+    $b = new B();
+    
+    $c = clone $b;
+?>
